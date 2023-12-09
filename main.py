7703f905c19059acddef4544fcef922300fb6158
Dean Rice <deanrice44@gmail.com>
2023-12-09 00:58:39 -0500

    Basic functionality done

--- main.py
+++ main.py
@@ -1,33 +1,120 @@
 import pyautogui
-import easyocr
+import keyboard
+import csv
+from tesseract_ocr import TesseractManager
+from time import sleep
 
 IMAGES = "bin/images/"
-ocr = easyocr.Reader(['en'])
+BANLIST = "bin/banlist.csv"
+OFFSET_X = (210, 260)
+OFFSET_Y = (240 , 800)
+SLEEP_INTERVAL = .1
 
 
 class GuiManager():
 
     def __init__(self):
         self.screenWidth, self.screenHeight = pyautogui.size()
-        self.update_mouse()
+        self.banlist = []
+        self.exit = False
+        self.update_banlist()
+        
+    def update_banlist(self):
+        with open(BANLIST, newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                self.banlist.append(str(row[0]))
+        
+    def add_to_banlist(self, name=None, names=[]):
+        if len(names) > 0:
+            with open(BANLIST, "w", newline='') as csvfile:
 
-    def update_mouse(self):
-        self.mouseX, self.mouseY = pyautogui.position()
+                writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                for n in names:
+                    writer.writerow(n)
+
+        else:
+            with open(BANLIST, "w", newline='') as csvfile:
+                writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                writer.writerow(name)
+
+        
+        self.update_banlist()
+
 
     def get_names(self):
-        tmp_filename = f"{IMAGES}test_namelist.png"
+        tmp_filename = f"{IMAGES}namelist.png"
 
         #Hardcoded coordinates for name column
-        x1, y1 = 210, 260
-        x2, y2 = 380, 1066
+        x1, y1 = OFFSET_X[0], OFFSET_X[1]
+        x2, y2 = OFFSET_Y[0], OFFSET_Y[1]
         
-        #name_img = pyautogui.screenshot(tmp_filename,region=(x1,y1,x2,y2))
-        names = ocr.readtext(tmp_filename, detail = 0)
-        print(names)
-       
+        pyautogui.screenshot(tmp_filename,region=(x1,y1,x2,y2))
+        print("Screenshot taken!")
+        #names = ocr.readtext(tmp_filename, detail = 0)
+        #print(names)
+
+    #Passes in one of the tuples from t.read_image()
+    #Assumes the tuple has been verified as a user who needs to be kicked
+    def click_and_kick(self, user:tuple):
+        avg_x = ((user[0][2] + user[0][0])/2) + OFFSET_X[0]
+        avg_y = ((user[0][3] + user[0][1])/2) + OFFSET_Y[0]
+        print(f"Average X: {avg_x}\nAverage Y: {avg_y}")
+        #Make sure SC2 is in focus then right click
+        pyautogui.click(x = avg_x, y = avg_y, button="left")
+        pyautogui.click(x = avg_x, y = avg_y, button="right")
+        sleep(.03)
+        kick_coords = pyautogui.center(pyautogui.locateOnScreen(f"{IMAGES}button.png", confidence=.95))
+        pyautogui.click(x = kick_coords.x, y = kick_coords.y, button = "left")
+        
+
+    
+    
+    def kick_from_blacklist(self):
+   
+        t = TesseractManager(f"{IMAGES}namelist.png", .5)
+
+        #g.get_names()
+        result = t.read_image()
+        keys = []
+        blacklist_chars = ["<",">","."]
+        for key in result.keys():
+            print(result[key])
+            keys.append(key)
+        print(keys)
+        keys_to_remove = []
+        print(f"Banlist: {self.banlist}")
+        for key in keys:
+            if any(char in key for char in blacklist_chars) or key == " " or key not in self.banlist:
+                keys_to_remove.append(key)
+
+        for key in keys_to_remove:
+            del result[key]
+            keys.remove(key)
+
+        print(keys)
+        g.click_and_kick(result[keys[0]])
+
+    def disable_loop(self, e):
+        self.exit = False
+    
+    def mainloop(self):
+        keyboard.add_hotkey("ctrl+n", self.disable_loop)
+        keyboard.add_hotkey("ctrl+r", self.update_banlist)
+        self.add_to_banlist(names=[["TheKilla"], ["Procyon"], ["Killagal"], ["HighVoltage"],["Markus"],["Yugo"],["cat"]])
+        input("Init done. Press enter to begin.")
+        self.exit = False
+        while not self.exit:
+            self.kick_from_blacklist()
+            sleep(SLEEP_INTERVAL)
+            #TheKilla, Procyon, Killagal, HighVoltage, Markus, Yugo, LegIt
+
+        print("Done!")
+    
+
 
 if __name__ == "__main__":
     g = GuiManager()
-
-    g.get_names()
+    g.mainloop()
+        
 
---
+++ tesseract_ocr.py
@@ -0,0 +1,41 @@
+from pytesseract import Output
+import pytesseract
+import argparse
+import cv2
+
+class TesseractManager():
+
+    def __init__(self, img="", min_conf=0):
+       self.image = img
+       self.min_conf = min_conf
+       
+
+    def read_image(self):
+        image = cv2.imread(self.image)
+        rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        result = pytesseract.image_to_data(rgb, output_type=Output.DICT)
+
+        #Tuple containing confidence and coordinates
+        ret = {}
+        for i in range(0, len(result["text"])):
+            x = result["left"][i]
+            y = result["top"][i]
+            w = result["width"][i]
+            h = result["height"][i]
+
+            text = result["text"][i]
+            conf = int(result["conf"][i])
+
+            if conf > self.min_conf:
+                #print(f"Confidence: {conf}")
+                #print(f"Text: {text}") 
+                #print("===================")
+                ret[text] = ([x, y, x + w, y + h], conf)
+                text = "".join([c if ord(c) < 128 else "" for c in text]).strip()
+                #cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                #cv2.putText(image, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                #            1.2, (0, 0, 255), 3)
+                
+        return ret
+        #cv2.imshow("Image", image)
+        #cv2.waitKey(0)
