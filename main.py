861a94afb8cf7db8e9a89f9dbdf1ecae8af3bdec
Dean Rice <deanrice44@gmail.com>
2023-12-10 23:26:21 -0500

    Added pause, cleaned up some functions

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import keyboard
 import csv
 from tesseract_ocr import TesseractManager
 from time import sleep
+from time import perf_counter
 
 IMAGES = "bin/images/"
 BANLIST = "bin/banlist.csv"
@@ -17,6 +18,7 @@ class GuiManager():
         self.screenWidth, self.screenHeight = pyautogui.size()
         self.banlist = []
         self.exit = False
+        self.pause = False
         self.update_banlist()
         
     def update_banlist(self):
@@ -50,22 +52,20 @@ class GuiManager():
         x2, y2 = OFFSET_Y[0], OFFSET_Y[1]
         
         pyautogui.screenshot(tmp_filename,region=(x1,y1,x2,y2))
-        print("Screenshot taken!")
-        #names = ocr.readtext(tmp_filename, detail = 0)
-        #print(names)
 
     #Passes in one of the tuples from t.read_image()
     #Assumes the tuple has been verified as a user who needs to be kicked
     def click_and_kick(self, user:tuple):
         avg_x = ((user[0][2] + user[0][0])/2) + OFFSET_X[0]
         avg_y = ((user[0][3] + user[0][1])/2) + OFFSET_Y[0]
-        print(f"Average X: {avg_x}\nAverage Y: {avg_y}")
-        #Make sure SC2 is in focus then right click
-        pyautogui.click(x = avg_x, y = avg_y, button="left")
+
         pyautogui.click(x = avg_x, y = avg_y, button="right")
-        sleep(.03)
-        kick_coords = pyautogui.center(pyautogui.locateOnScreen(f"{IMAGES}button.png", confidence=.95))
-        pyautogui.click(x = kick_coords.x, y = kick_coords.y, button = "left")
+        sleep(.02)
+        try:
+            kick_coords = pyautogui.center(pyautogui.locateOnScreen(f"{IMAGES}button.png", confidence=.95))
+            pyautogui.click(x = kick_coords.x, y = kick_coords.y, button = "left")
+        except pyautogui.ImageNotFoundException:
+            print("Kick button not found on screen! Continuing...")
         
 
     
@@ -74,16 +74,17 @@ class GuiManager():
    
         t = TesseractManager(f"{IMAGES}namelist.png", .5)
 
-        #g.get_names()
+        g.get_names()
         result = t.read_image()
         keys = []
+        #Removes clan tags and the AI
+        #Idk if these chars are banned in names tho is the problem
         blacklist_chars = ["<",">","."]
         for key in result.keys():
             print(result[key])
             keys.append(key)
         print(keys)
         keys_to_remove = []
-        print(f"Banlist: {self.banlist}")
         for key in keys:
             if any(char in key for char in blacklist_chars) or key == " " or key not in self.banlist:
                 keys_to_remove.append(key)
@@ -93,21 +94,36 @@ class GuiManager():
             keys.remove(key)
 
         print(keys)
-        g.click_and_kick(result[keys[0]])
+        if len(keys) > 0:
+            g.click_and_kick(result[keys[0]])
+        else:
+            print("No targets found.")
 
-    def disable_loop(self, e):
-        self.exit = False
+    def disable_loop(self):
+        self.exit = True
+
+    def pause_loop(self):
+        if self.pause:
+            print("Unpausing...")
+        else:
+            print("Pausing...")
+        self.pause = not self.pause
     
     def mainloop(self):
         keyboard.add_hotkey("ctrl+n", self.disable_loop)
         keyboard.add_hotkey("ctrl+r", self.update_banlist)
-        self.add_to_banlist(names=[["TheKilla"], ["Procyon"], ["Killagal"], ["HighVoltage"],["Markus"],["Yugo"],["cat"]])
+        keyboard.add_hotkey("ctrl+p", self.pause_loop)
         input("Init done. Press enter to begin.")
         self.exit = False
         while not self.exit:
+            s_time = perf_counter()
             self.kick_from_blacklist()
+            e_time = perf_counter()
             sleep(SLEEP_INTERVAL)
-            #TheKilla, Procyon, Killagal, HighVoltage, Markus, Yugo, LegIt
+
+            while self.pause:
+                sleep(.5)
+            
 
         print("Done!")
     
